4911ef89fbfb3b4bf699ffdc7f51f14e70a4cbb6
Pablo  Yeverino <pabloyeverino@Pablos-MacBook-Pro.local>
2023-04-05 15:48:52 -0700

    Migrate createStore to configureStore from Redux Tool Kit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
 import { createStore } from 'redux';
 import cakeReducer from './cake/cakeReducer';
+import { configureStore } from '@reduxjs/toolkit';
 
-const store = createStore(cakeReducer);
+const store = configureStore({
+    reducer: cakeReducer
+});
 
 export default store;
