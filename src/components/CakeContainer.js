79fa0d1eec3c448edda141803e338feae1c122bd
Pablo  Yeverino <pabloyeverino@Pablos-MacBook-Pro.local>
2023-04-05 15:29:22 -0700

    Create react app and add Components folder and CakeContainer component'

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CakeContainer() {
+    return (
+        <>
+            <h2>Number of cakes</h2>
+            <button>Buy Cake</button>
+        </>
+    )
+}
+
+export default CakeContainer;
